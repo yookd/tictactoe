3e6a0121e2ad21990c45e3e1e77b9722ec43616f
David Yoo <david@ribbon.co>
2016-03-28 22:33:04 -0700

    Client entry point for app

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,27 @@
-import React       from 'react';
-import { render }  from 'react-dom';
-import { Router }  from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-import routes      from 'routes';
+import React                            from 'react';
+import { render }                       from 'react-dom';
+import { Router, browserHistory }       from 'react-router';
+import routes                           from 'routes';
+import { createStore, combineReducers } from 'redux';
+import { Provider }                     from 'react-redux';
+import { fromJS }                       from 'immutable';
+import * as reducers                    from 'reducers';
 
-const history = createBrowserHistory();
+let initialState = window.__INITIAL_STATE__ || {};
+
+// Transform into Immutable.js collections
+Object
+  .keys(initialState)
+  .forEach(key => {
+    initialState[key] = fromJS(initialState[key]);
+  });
+
+const reducer = combineReducers(reducers);
+const store   = createStore(reducer, initialState);
 
 render((
-  <Router children={ routes } history={ history } />
-  ), document.getElementById('react-view')
+  <Provider store={ store }>
+    <Router children={ routes } history={ browserHistory } />
+  </Provider>
+  ), document.getElementById('main')
 );
